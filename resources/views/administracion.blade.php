0c24e70165c93db538676a88b710b443fede95c1
WilbergerMatias <matias180399@gmail.com>
2023-04-28 09:49:29 -0300

    Modificacion N° 12: se agrego baja logica a algunas tablas, se creo pagina principal para administracion (blade)

---
+++ resources/views/administracion.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>WebCines</title>
+
+    </head>
+    <body>
+        <input type="submit" action="" value="Generos">
+        <input type="submit" action="" value="Funcion">
+        <input type="submit" action="" value="Pelicula">    
+    </body>
+</html>
