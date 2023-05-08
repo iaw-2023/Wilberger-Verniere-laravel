c4262fb3445f3a6012a8c32d88abc3de4215f004
WilbergerMatias <matias180399@gmail.com>
2023-05-08 10:29:49 -0300

    Modificacion N°23: Se estilizo la pagina de administracion, se agregaron las imagenes de fondo al repositorio del proyecto

--- resources/views/administracion.blade.php
+++ resources/views/administracion.blade.php
@@ -3,22 +3,32 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+        @vite(['resources/css/administracion.css'])
         <title>WebCines</title>
-
     </head>
-    <body>
-        <form action="{{ route('genero.index') }}" method="Post">
-            <a class="btn btn-primary" href="{{ route('genero.index')}}">Generos</a>
-        </form>        
-        <form action="" method="Post">
-            <a class="btn btn-primary" href="">Funciones</a>
-        </form> 
-        <form action="" method="Post">
-            <a class="btn btn-primary" href="">Peliculas</a>
-        </form> 
-        <form action="{{ route('sala.index') }}" method="Post">
-            <a class="btn btn-primary" href="{{ route('sala.index') }}">Salas</a>
-        </form> 
+    <body >
+        <div class="wrapper" style="background-image: url({{asset('/../images/movie_background.jpg')}});">
+            <div class="item1">    
+                <form action="{{ route('genero.index') }}" method="Post">
+                    <a class="btn btn-secondary btn-generos" href="{{ route('genero.index')}}">Generos</a>
+                </form>
+            </div>        
+            <div class="item2">    
+                <form action="" method="Post">
+                    <a class="btn btn-secondary btn-peliculas" href="">Peliculas</a>
+                </form>
+            </div>
+            <div class="item3">
+                <form action="" method="Post">
+                    <a class="btn btn-secondary btn-funciones" href="">Funciones</a>
+                </form> 
+            </div>
+            <div class="item4">    
+                <form action="{{ route('sala.index') }}" method="Post">
+                    <a class="btn btn-secondary btn-salas" href="{{ route('sala.index') }}">Salas</a>
+                </form> 
+            </div>    
+        </div>
     </body>
 </html>
