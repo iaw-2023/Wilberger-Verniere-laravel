8651d9ac1a763e8db61ade2dd3e305d580435d7f
MartinVerniere <martincho7verniere@yahoo.com>
2023-05-21 18:13:39 -0300

    Agregada funcionalidad del sidebar
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,19 @@
+<a href="{{ url('/administracion') }}">Inicio</a>
+
+
+<a href="{{ route('funcion.index') }}">Funciones</a>
+<a href="{{ route('funcion.create') }}" class="añadir">Añadir</a>
+
+<a href="{{ route('pelicula.index') }}">Peliculas</a>
+<a href="{{ route('pelicula.create') }}" class="añadir">Añadir</a>
+
+<a href="{{ route('genero.index') }}">Generos</a>
+<a href="{{ route('genero.create') }}" class="añadir">Añadir</a>
+
+<a href="{{ route('sala.index') }}">Salas</a>
+<a href="{{ route('sala.create') }}" class="añadir">Añadir</a>
+
+<a href="{{ route('compra.index') }}">Compras</a>
+
+
+<a href="{{ route('compra.index') }}">Log Out</a>
