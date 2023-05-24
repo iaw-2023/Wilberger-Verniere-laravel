ece28f793f11781127aa325111b4be92e10ee6d2
MartinVerniere <martincho7verniere@yahoo.com>
2023-05-24 15:06:23 -0300

    Update GeneroCollection.php

--- app/Http/Resources/GeneroCollection.php
+++ app/Http/Resources/GeneroCollection.php
@@ -15,7 +15,7 @@ class GeneroCollection extends ResourceCollection
     public function toArray(Request $request): array
     {
         return [
-            'Nombre' => $this->nombre,
+            'Nombre' => $request->nombre,
         ];
     }
 }
