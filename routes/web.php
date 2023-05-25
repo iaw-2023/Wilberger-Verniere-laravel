1327f17ed00d90d69a26a50e0642a2134f62df49
MartinVerniere <martincho7verniere@yahoo.com>
2023-05-25 15:43:25 -0300

    Añadidas rutas habiltiar/deshabilitar en web.php

--- routes/web.php
+++ routes/web.php
@@ -40,25 +40,48 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//PREGUNTAR PROFESOR MIDDLEWARE
 Route::resource('genero', GeneroController::class)->middleware(Authenticate::Class);
+Route::post('genero/habilitar', [GeneroController::class, 'habilitar'])
+    ->name('genero.habilitar')
+    ->middleware(Authenticate::Class);
+Route::post('genero/deshabilitar', [GeneroController::class, 'deshabilitar'])
+    ->name('genero.deshabilitar')
+    ->middleware(Authenticate::Class);
 
 Route::resource('pelicula', PeliculaController::class)->middleware(Authenticate::Class);
+Route::post('pelicula/habilitar', [PeliculaController::class, 'habilitar'])
+    ->name('pelicula.habilitar')
+    ->middleware(Authenticate::Class);
+Route::post('pelicula/deshabilitar', [PeliculaController::class, 'deshabilitar'])
+    ->name('pelicula.deshabilitar')
+    ->middleware(Authenticate::Class);
 
 Route::resource('funcion', FuncionController::class)->middleware(Authenticate::Class);
-Route::post('funcion', [FuncionController::class, 'habilitar'])
+Route::post('funcion/habilitar', [FuncionController::class, 'habilitar'])
     ->name('funcion.habilitar')
     ->middleware(Authenticate::Class);
-Route::post('funcion', [FuncionController::class, 'deshabilitar'])
+Route::post('funcion/deshabilitar', [FuncionController::class, 'deshabilitar'])
     ->name('funcion.deshabilitar')
     ->middleware(Authenticate::Class);
 
 Route::resource('sala', SalaController::class)->middleware(Authenticate::Class);
+Route::post('sala/habilitar', [SalaController::class, 'habilitar'])
+    ->name('sala.habilitar')
+    ->middleware(Authenticate::Class);
+Route::post('sala/deshabilitar', [SalaController::class, 'deshabilitar'])
+    ->name('sala.deshabilitar')
+    ->middleware(Authenticate::Class);
 
 Route::resource('compra', CompraController::class)->middleware(Authenticate::Class);
 
 Route::resource('detallesCompra', DetallesCompraController::class)->middleware(Authenticate::Class);
 
+
+
+
 Route::view('/swagger', 'swagger');
+
+
+
 
 require __DIR__.'/auth.php';
