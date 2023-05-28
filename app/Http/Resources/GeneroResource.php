870360a8decaa63824d6f1f0d488b3970abce199
MartinVerniere <martincho7verniere@yahoo.com>
2023-05-28 16:52:22 -0300

    Update GeneroResource.php
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- app/Http/Resources/GeneroResource.php
+++ app/Http/Resources/GeneroResource.php
@@ -5,6 +5,8 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\DB;
+use App\Http\Resources\PeliculaCollection;
+use App\Models\Pelicula;
 
 class GeneroResource extends JsonResource
 {
