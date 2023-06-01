12b11c71d9f1ae76208d916f33f7eb05ca375d06
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-01 14:22:28 -0300

    Se elimino import innecesario

--- app/Http/Resources/GeneroResource.php
+++ app/Http/Resources/GeneroResource.php
@@ -5,7 +5,6 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\DB;
-use App\Http\Resources\PeliculaCollection;
 use App\Models\Pelicula;
 
 class GeneroResource extends JsonResource
