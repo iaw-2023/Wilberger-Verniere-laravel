5a9c31a3cfcd97f8051dba3235e0e36f9cb384da
WilbergerMatias <matias180399@gmail.com>
2023-06-02 11:05:22 -0300

    se realizo cambios para testear en el deploy de vercel

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,13 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/master.css',
                 'resources/js/app.js',
-                'resources/js/swagger.js'
+                'resources/js/swagger.js',
+                'resources/js/sidebar-toggle.js',
+                'resources/views/master.blade.php',
+                'resources/views/sidebar.blade.php',
+                'resources/views/swagger.blade.php',
             ],
             refresh: true,
         }),
