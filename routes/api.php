15b98c377627d814a18be69971d3371576c85596
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-09 18:02:30 -0300

    Se elimino CORS.php, ya no lo usamos
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -23,18 +23,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/generos', [APIGeneroController::class, 'index'])->middleware(CORS::Class);
-Route::get('/generos/{idGenero}', [APIGeneroController::class, 'show'])->middleware(CORS::Class);
+Route::get('/generos', [APIGeneroController::class, 'index']);
+Route::get('/generos/{idGenero}', [APIGeneroController::class, 'show']);
 
-Route::get('/peliculas', [APIPeliculaController::class, 'index'])->middleware(CORS::Class);
-Route::get('/peliculas/{idPelicula}', [APIPeliculaController::class, 'show'])->middleware(CORS::Class);
+Route::get('/peliculas', [APIPeliculaController::class, 'index']);
+Route::get('/peliculas/{idPelicula}', [APIPeliculaController::class, 'show']);
 
-Route::get('/funciones', [APIFuncionController::class, 'index'])->middleware(CORS::Class);
-Route::get('/funciones/{idFuncion}', [APIFuncionController::class, 'show'])->middleware(CORS::Class);
+Route::get('/funciones', [APIFuncionController::class, 'index']);
+Route::get('/funciones/{idFuncion}', [APIFuncionController::class, 'show']);
 
-Route::get('/compras', [APICompraController::class, 'index'])->middleware(CORS::Class);
-Route::get('/compras/{idCompra}', [APICompraController::class, 'show'])->middleware(CORS::Class);
-Route::post('/compras/crear', [APICompraController::class, 'store'])->middleware(CORS::Class);
-Route::delete('/compras/eliminar', [APICompraController::class, 'destroy'])->middleware(CORS::Class);
+Route::get('/compras', [APICompraController::class, 'index']);
+Route::get('/compras/{idCompra}', [APICompraController::class, 'show']);
+Route::post('/compras/crear', [APICompraController::class, 'store']);
+Route::delete('/compras/eliminar', [APICompraController::class, 'destroy']);
 
-Route::get('/detallesCompras/{idDetallesCompra}', [APIDetallesCompraController::class, 'show'])->middleware(CORS::Class);
+Route::get('/detallesCompras/{idDetallesCompra}', [APIDetallesCompraController::class, 'show']);
