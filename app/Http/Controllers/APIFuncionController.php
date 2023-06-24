6a51e37299e264dc548fb4d58a97c782093a95c1
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-24 18:06:26 -0300

    Update APIFuncionController.php
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- app/Http/Controllers/APIFuncionController.php
+++ app/Http/Controllers/APIFuncionController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Resources\FuncionResource;
 use App\Models\Funcion;
 use App\Models\Pelicula;
+use Illuminate\Database\Eloquent\Builder;
 
 class APIFuncionController extends Controller
 {
