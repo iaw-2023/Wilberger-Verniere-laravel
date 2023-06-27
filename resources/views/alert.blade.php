7be8b829cff8f9088d4fecb981111234eb552ade
MartinVerniere <martincho7verniere@yahoo.com>
2023-06-27 17:45:45 -0300

    Update alert.blade.php
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -1,16 +1,10 @@
 @if ($message = Session::get('Success'))
     <div class="alert alert-success alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert">
-            <i class="fa fas-times"></i>
-        </button>
         <p>{{ $message }}</p>
     </div>
 @endif
-@if ($message = Session::has('Error'))
+@if ($message = Session::get('Error'))
     <div class="alert alert-danger alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert">
-            <i class="fa fas-times"></i>
-        </button>
         <p>{{ $message }}</p>
     </div>
 @endif
