f227f18199f2cb466347bc4a85775b1ae7b5ad03
MartinVerniere <martincho7verniere@yahoo.com>
2023-08-26 18:56:15 -0300

    Se añadio APIController para usuario
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

---
+++ app/Http/Controllers/APIUsuarioController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsuarioAPIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $email = $request->query('Email');
+        $contraseña = $request->query('Contraseña');
+        $nombre = $request->query('Nombre');
+        $validarEmail = $request->validate([
+            $email => 'exists:usuario,email',
+        ]);
+        $validarNombre = $request->validate([
+            $nombre => 'exists:usuario,nombre',
+        ]);
+
+        if (!$validarEmail && !$validarNombre){
+            User::agregarUsuario($email,$contraseña,$nombre);
+            return response()->json(['success' => 'Se creo el nuevo usuario'], 200);
+        }
+        else{
+            return response()->json(['error' => 'No se creo el nuevo usuario'], 400);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/usuarios', [APIUsuarioController::class, 'store']);
+
 Route::get('/generos', [APIGeneroController::class, 'index']);
 Route::get('/generos/{idGenero}', [APIGeneroController::class, 'show']);
 
