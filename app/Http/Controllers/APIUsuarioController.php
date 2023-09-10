d7502f303b399f2c7d6aae90dc958b943c43d4f0
MartinVerniere <martincho7verniere@yahoo.com>
2023-09-10 16:43:48 -0300

    Update APIUsuarioController.php
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- app/Http/Controllers/APIUsuarioController.php
+++ app/Http/Controllers/APIUsuarioController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
-class UsuarioAPIController extends Controller
+class APIUsuarioController extends Controller
 {
     /**
      * Display a listing of the resource.
