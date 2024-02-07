7f28aa2b1867bf051e5d6bdbad89812a051ba2d2
MartinVerniere <martincho7verniere@yahoo.com>
2024-02-07 15:24:20 -0300

    Agregados nuevos usuarios basicos
    Cada uno con un rol distinto

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,10 +17,31 @@ class UserSeeder extends Seeder
     {
         //
         DB::table('users')->insert([
-            'name' => "admin",
+            'name' => "Admin",
             'email'=>"admin@iaw.com",            
             'password'=>Hash::make('admin123'),
             'rol'=>"SUPER_ADMIN",
-        ]);
+        ])->assignRole('superadmin');
+
+        DB::table('users')->insert([
+            'name' => "FuncionAdmin",
+            'email'=>"funcionAdmin@iaw.com",            
+            'password'=>Hash::make('admin234'),
+            'rol'=>"ADMIN",
+        ])->assignRole('adminFunciones');
+
+        DB::table('users')->insert([
+            'name' => "SalaAdmin",
+            'email'=>"salaAdmin@iaw.com",            
+            'password'=>Hash::make('admin456'),
+            'rol'=>"ADMIN",
+        ])->assignRole('adminSalas');
+
+        DB::table('users')->insert([
+            'name' => "GenyPelAdmin",
+            'email'=>"generoypeliculaAdmin@iaw.com",            
+            'password'=>Hash::make('admin567'),
+            'rol'=>"ADMIN",
+        ])->assignRole('adminGenerosPeliculas');
     }
 }
