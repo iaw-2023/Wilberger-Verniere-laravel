e0a6bc3b207d96260a37d0294b2dbcf0d69cb8e3
MartinVerniere <martincho7verniere@yahoo.com>
2024-02-10 16:53:13 -0300

    Ahora las rutas usan middleware
    Co-Authored-By: WilbergerMatias <71281424+WilbergerMatias@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -40,45 +40,37 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('genero', GeneroController::class)->middleware(Authenticate::Class);
-Route::put('genero/habilitar/{id}', [GeneroController::class, 'habilitar'])
-    ->name('genero.habilitar')
-    ->middleware(Authenticate::Class);
-Route::delete('genero/deshabilitar/{id}', [GeneroController::class, 'deshabilitar'])
-    ->name('genero.deshabilitar')
-    ->middleware(Authenticate::Class);
+Route::middleware('auth')->group(function () {
+    Route::resource('genero', GeneroController::class);
+    Route::put('genero/habilitar/{id}', [GeneroController::class, 'habilitar'])
+        ->name('genero.habilitar');
+    Route::delete('genero/deshabilitar/{id}', [GeneroController::class, 'deshabilitar'])
+        ->name('genero.deshabilitar');
 
-Route::resource('pelicula', PeliculaController::class)->middleware(Authenticate::Class);
-Route::put('pelicula/habilitar/{id}', [PeliculaController::class, 'habilitar'])
-    ->name('pelicula.habilitar')
-    ->middleware(Authenticate::Class);
-Route::delete('pelicula/deshabilitar/{id}', [PeliculaController::class, 'deshabilitar'])
-    ->name('pelicula.deshabilitar')
-    ->middleware(Authenticate::Class);
+    Route::resource('pelicula', PeliculaController::class);
+    Route::put('pelicula/habilitar/{id}', [PeliculaController::class, 'habilitar'])
+        ->name('pelicula.habilitar');
+    Route::delete('pelicula/deshabilitar/{id}', [PeliculaController::class, 'deshabilitar'])
+        ->name('pelicula.deshabilitar');
 
-Route::resource('funcion', FuncionController::class)->middleware(Authenticate::Class);
-Route::put('funcion/habilitar/{id}', [FuncionController::class, 'habilitar'])
-    ->name('funcion.habilitar')
-    ->middleware(Authenticate::Class);
-Route::delete('funcion/deshabilitar/{id}', [FuncionController::class, 'deshabilitar'])
-    ->name('funcion.deshabilitar')
-    ->middleware(Authenticate::Class);
+    Route::resource('funcion', FuncionController::class);
+    Route::put('funcion/habilitar/{id}', [FuncionController::class, 'habilitar'])
+        ->name('funcion.habilitar');
+    Route::delete('funcion/deshabilitar/{id}', [FuncionController::class, 'deshabilitar'])
+        ->name('funcion.deshabilitar');
 
-Route::resource('sala', SalaController::class)->middleware(Authenticate::Class);
-Route::put('sala/habilitar/{id}', [SalaController::class, 'habilitar'])
-    ->name('sala.habilitar')
-    ->middleware(Authenticate::Class);
-Route::delete('sala/deshabilitar/{id}', [SalaController::class, 'deshabilitar'])
-    ->name('sala.deshabilitar')
-    ->middleware(Authenticate::Class);
+    Route::resource('sala', SalaController::class);
+    Route::put('sala/habilitar/{id}', [SalaController::class, 'habilitar'])
+        ->name('sala.habilitar');
+    Route::delete('sala/deshabilitar/{id}', [SalaController::class, 'deshabilitar'])
+        ->name('sala.deshabilitar');
 
-Route::resource('compra', CompraController::class)->middleware(Authenticate::Class);
+    Route::resource('compra', CompraController::class);
 
-Route::resource('detallesCompra', DetallesCompraController::class)->middleware(Authenticate::Class);
-Route::get('detallesCompra/listaOrdenes/{idCompra}', [DetallesCompraController::class, 'ordenesAsociadas'])
-    ->name('detallesCompra.ordenesAsociadas')
-    ->middleware(Authenticate::Class);
-
+    Route::resource('detallesCompra', DetallesCompraController::class);
+    Route::get('detallesCompra/listaOrdenes/{idCompra}', [DetallesCompraController::class, 'ordenesAsociadas'])
+        ->name('detallesCompra.ordenesAsociadas');   
+});
 
 
 
