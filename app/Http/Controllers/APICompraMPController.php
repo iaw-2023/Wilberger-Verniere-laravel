9d8bd8cb5124aefd2164be44d86dff6d34789c3b
MartinVerniere <martincho7verniere@yahoo.com>
2024-12-11 17:01:08 -0300

    Update APICompraMPController.php
    Co-Authored-By: Wilberger Matias <71281424+WilbergerMatias@users.noreply.github.com>

--- app/Http/Controllers/APICompraMPController.php
+++ app/Http/Controllers/APICompraMPController.php
@@ -35,9 +35,9 @@ class APICompraMPController extends Controller
                 "installments" => $body['installments'], //<INSTALLMENTS>
                 "payment_method_id" => $body['payment_method_id'], //<PAYMENT_METHOD_ID>
                 "issuer_id" => $body['issuer_id'], //<ISSUER>
+                "cardholderName" => $body['cardholderName'], //<CARD_HOLDER_NAME>
                 "payer" => [
-                    "email" => $body['payer']['email'], //<EMAIL>
-                    "card_holder_name" => $body['payer']['card_holder_name'], //<CARD_HOLDER_NAME>
+                    "email" => $body['payer']['email'], //<EMAIL> 
                     "identification" => [
                         "type" => $body['payer']['identification']['type'], //<IDENTIFICATION_TYPE
                         "number" => $body['payer']['identification']['number'] //<NUMBER>
